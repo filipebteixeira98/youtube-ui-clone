c3d32f60c13b3124d0f3dfb7359f145c1a11cc1a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-05-29 21:12:41 -0300

    refactor: changed header component structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineSearch } from 'react-icons/ai'
 import { BsThreeDotsVertical } from 'react-icons/bs'
+import { FiCast } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -9,16 +10,31 @@ export function Header() {
   return (
     <header className={styles.header__container}>
       <div className={styles.header__content}>
-        <img src={logoImg} alt="Youtube Music" />
-        <nav>
-          <a href="#home">Home</a>
-          <a href="#explore">Explore</a>
-          <a href="#library">Library</a>
-          <a href="#search">Search</a>
-        </nav>
         <div>
-          <AiOutlineSearch size={24} />
-          <BsThreeDotsVertical size={24} />
+          <a href="#home">
+            <img src={logoImg} alt="Youtube Music" />
+          </a>
+        </div>
+        <div>
+          <nav>
+            <div className={styles.header__nav}>
+              <a href="#home">Home</a>
+              <a href="#explore">Explore</a>
+              <a href="#library">Library</a>
+              <div>
+                <AiOutlineSearch size={20} />
+                <a href="#search">Search</a>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div>
+          <a href="#cast">
+            <FiCast size={24} />
+          </a>
+          <a href="#options">
+            <BsThreeDotsVertical size={24} />
+          </a>
           <button type="button">SIGN IN</button>
         </div>
       </div>
