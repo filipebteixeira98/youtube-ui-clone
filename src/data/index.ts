30f7a9708b0fcf7ba0b25dd948c35ee94559150d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-05-31 15:36:41 -0300

    chore: added data to consume

---
+++ src/data/index.ts
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    url: 'https://images.genius.com/038c44a69f400cb317d469e4bdd0231e.1000x1000x1.png',
+    title: 'M4',
+    album: 'M4',
+    artist: 'Teto & Matuê',
+  },
+  {
+    url: 'https://studiosol-a.akamaihd.net/letras/272x272/albuns/e/9/b/1/1047941616175829.jpg',
+    title: 'Além do Dinheiro',
+    album: 'Imaterial',
+    artist: 'Filipe Ret, Dallass & Ariel Donato',
+  },
+  {
+    url: 'https://i.scdn.co/image/ab67616d00001e02e2e352d89826aef6dbd5ff8f',
+    title: 'Sunflower (Spider-Man: Into the Spider-Verse)',
+    album: 'Spider-Man: Into the Spider-Verse',
+    artist: 'Post Malone, Swae Lee &',
+  },
+  {
+    url: 'https://s2.glbimg.com/mjhMXhUJZYvzK3RVO-O-eVm45AY=/620x520/smart/e.glbimg.com/og/ed/f/original/2021/11/30/malvadao3-2.png',
+    title: 'Malvadão 3',
+    album: 'Malvadão 3',
+    artist: 'Xamã, Gustah & Neo Beats',
+  },
+]
